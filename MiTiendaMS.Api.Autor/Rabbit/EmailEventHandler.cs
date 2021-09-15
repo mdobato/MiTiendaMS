a01f7a03f7dccb45b340793fa16f4d2c494bda79
Leonardo Da Vinci <mdobato@gmail.com>
2021-09-15 19:02:45 +0200

    Add broker message (Rabbit queue) to solution

---
+++ MiTiendaMS.Api.Autor/Rabbit/EmailEventHandler.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.Logging;
+using MiTiendaMS.RabbitMQ.Bus.BusRabbit;
+using MiTiendaMS.RabbitMQ.Bus.Queue;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MiTiendaMS.Api.Autor.Rabbit
+{
+    public class EmailEventHandler : IEventHandler<EmailQueueEvent>
+    {
+        private readonly ILogger<EmailEventHandler> _logger;
+
+        public EmailEventHandler() {}
+
+        public EmailEventHandler(ILogger<EmailEventHandler> logger)
+        {
+            _logger = logger;
+
+        }
+
+        public Task Handle(EmailQueueEvent evt)
+        {
+            _logger.LogInformation($"Mensaje de la subscripción al bus: {evt.Subject} {evt.Title}");
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ MiTiendaMS.RabbitMQ.Bus/Events/Event.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MiTiendaMS.RabbitMQ.Bus.Events
+{
+    public abstract class Event
+    {
+        public DateTime Timestamp { get; protected set; }
+
+        public Event()
+        {
+            Timestamp = DateTime.Now;
+        }
+    }
+}
